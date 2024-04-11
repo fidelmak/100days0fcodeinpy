504479e196671608e478d76dd4f1cddad92c3a2c
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2024-04-11 15:05:12 +0100

    update

--- day1/print.py
+++ day1/print.py
@@ -1,1 +1,23 @@
-print("welcome to python code")
+# print("welcome to python code")
+# print(" will this get me a Job ")
+# print(" just wanna understand this better ")
+# print(" will she be available \"Tomorrow\" and be fine ") # this is a double quote in string 
+# print(" this is another \nline  in python ")
+# print(" this is a concatenation " + " in python ")
+# # using the input function 
+# print("the world loves "  + input("  hello whats your favorite sports ") )
+# print("...................................")
+# print("Hi newbee")
+# message = input("please type your name ")
+# print(f" hello, my name is paul and you are {message}")
+
+a = 1
+b = 2
+
+c = a
+a = b
+b = c
+
+
+
+print(a)
